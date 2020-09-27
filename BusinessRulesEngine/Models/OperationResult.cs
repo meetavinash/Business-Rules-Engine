8c2887b938bb1d2c4db193f05551150ac2b752fa
Avinashh <68678361+meetavinash@users.noreply.github.com>
2020-09-28 01:42:49 +0530

    BusinessRuleEngine Project

---
+++ BusinessRulesEngine/Models/OperationResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusinessRulesEngine.Models
+{
+    public class OperationResult
+    {
+        public IEnumerable<string> Messages { get; set; }
+        public bool IsSuccessful { get { return !Messages.Any(); } }
+    }
+}
---
+++ BusinessRulesEngine/Models/OrderDetails.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusinessRulesEngine.Models
+{
+    public class OrderDetails
+    {        
+        public bool Paymentsuccessful { get; set; }
+        public Order OrderType { get; set; }
+    }
+
+    public enum Order
+    {
+        PhysicalProduct,
+        Book,
+        Membership,
+        Video
+    }
+
+    public enum MembershipStatus
+    {
+        New,
+        Upgrade
+    }
+}
